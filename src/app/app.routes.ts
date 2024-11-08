782ee2e0f5c0b01c90a95e3a8f0faaa821e0ce85
boonyong <boonyong.kang93@gmail.com>
2024-11-08 15:50:40 +0800

    Add about page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from '../home/home.component';
 import { PageNotFoundComponent } from '../page-not-found/page-not-found.component';
+import { AboutComponent } from '../about/about.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
   { path: '**', component: PageNotFoundComponent },
 ];
