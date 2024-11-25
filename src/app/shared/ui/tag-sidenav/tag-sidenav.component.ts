05f73da1707b36b0ac5afe0a0f7e390ba4cdf556
boonyong <boonyong.kang93@gmail.com>
2024-11-25 18:53:56 +0800

    Clean up styling - Highlight tag sidenav item based on filter. - Add tailwind custom component classes.

--- src/app/shared/ui/tag-sidenav/tag-sidenav.component.ts
+++ src/app/shared/ui/tag-sidenav/tag-sidenav.component.ts
@@ -4,15 +4,23 @@ import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { TagLinkComponent } from '../../../tags/ui/tag-link/tag-link.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
-    selector: 'app-tag-sidenav',
-    imports: [MatCardModule, MatButtonModule, MatIconModule, TagLinkComponent],
-    templateUrl: './tag-sidenav.component.html',
-    styleUrl: './tag-sidenav.component.css'
+  selector: 'app-tag-sidenav',
+  imports: [
+    CommonModule,
+    MatCardModule,
+    MatButtonModule,
+    MatIconModule,
+    TagLinkComponent,
+  ],
+  templateUrl: './tag-sidenav.component.html',
+  styleUrl: './tag-sidenav.component.css',
 })
 export class TagSidenavComponent {
   @Input() tags!: Tag[];
   @Input() more: boolean = false;
   @Input() loadMore!: () => void;
+  @Input() selectedTag = '';
 }
