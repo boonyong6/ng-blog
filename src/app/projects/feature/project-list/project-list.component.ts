972634482a899a469902ff3dba992f8636da0e41
boonyong <boonyong.kang93@gmail.com>
2024-11-26 16:33:04 +0800

    Add a button to fetch paginated projects

--- src/app/projects/feature/project-list/project-list.component.ts
+++ src/app/projects/feature/project-list/project-list.component.ts
@@ -1,23 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ProjectListItemComponent } from '../../ui/project-list-item/project-list-item.component';
 import { ProjectService } from '../../data-access/project.service';
-import { Observable } from 'rxjs';
-import { Page } from '../../../shared/data-access/types';
+import { Subject, takeUntil } from 'rxjs';
 import { Project } from '../../data-access/types';
-import { AsyncPipe } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-project-list',
-  imports: [AsyncPipe, ProjectListItemComponent],
+  imports: [MatButtonModule, MatIconModule, ProjectListItemComponent],
   templateUrl: './project-list.component.html',
   styleUrl: './project-list.component.css',
 })
-export class ProjectListComponent implements OnInit {
-  projectPage$!: Observable<Page<Project>>;
+export class ProjectListComponent implements OnInit, OnDestroy {
+  private destroyed = new Subject<void>();
+  projects: Project[] = [];
+  projectsNextUrl: string | null = null;
 
   constructor(private projectService: ProjectService) {}
 
   ngOnInit(): void {
-    this.projectPage$ = this.projectService.getProjects();
+    this.loadProjects();
+  }
+
+  ngOnDestroy(): void {
+    this.destroyed.next();
+    this.destroyed.complete();
+  }
+
+  loadMoreProjects() {
+    if (this.projectsNextUrl == null) {
+      return;
+    }
+    this.loadProjects(this.projectsNextUrl);
+  }
+
+  private loadProjects(url?: string): void {
+    this.projectService
+      .getProjects({ url })
+      .pipe(takeUntil(this.destroyed))
+      .subscribe((projectPage) => {
+        this.projects.push(...projectPage.results);
+        this.projectsNextUrl = projectPage.next;
+      });
   }
 }
