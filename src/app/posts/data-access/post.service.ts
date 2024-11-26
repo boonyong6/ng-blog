da6884aba56df2fc91590324234ad9bda1d8626f
boonyong <boonyong.kang93@gmail.com>
2024-11-26 15:42:13 +0800

    Clean up code

--- src/app/posts/data-access/post.service.ts
+++ src/app/posts/data-access/post.service.ts
@@ -5,6 +5,60 @@ import { Observable } from 'rxjs';
 import { Post } from './types';
 import { environment as env } from '../../../environments/environment';
 
+// TODO: Implement error handling.
+@Injectable({
+  providedIn: 'root',
+})
+export class PostService {
+  constructor(private http: HttpClient) {}
+
+  getLatestPosts(): Observable<Page<Post>> {
+    return this.http.get<Page<Post>>(`${env.apiBaseUrl}/posts/latest/`);
+  }
+
+  getPosts({ url, page = 1, tagSlug }: ListParams): Observable<Page<Post>> {
+    let endpoint = `${env.apiBaseUrl}/posts/?page=${page}`;
+
+    if (tagSlug) {
+      endpoint = `${env.apiBaseUrl}/tags/${tagSlug}/posts/?page=${page}`;
+    }
+
+    return this.http.get<Page<Post>>(url ? url : endpoint);
+  }
+
+  getPost({ url, year, month, day, slug }: GetParams): Observable<Post> {
+    return this.http.get<Post>(
+      url ? url : `${env.apiBaseUrl}/posts/${year}/${month}/${day}/${slug}/`,
+    );
+  }
+
+  getSimilarPosts({
+    url,
+    year,
+    month,
+    day,
+    slug,
+  }: GetParams): Observable<Page<Post>> {
+    return this.http.get<Page<Post>>(
+      url
+        ? url
+        : `${env.apiBaseUrl}/posts/${year}/${month}/${day}/${slug}/similar/`,
+    );
+  }
+
+  getTags({ url, page = 1 }: ListParams): Observable<Page<Tag>> {
+    return this.http.get<Page<Tag>>(
+      url ? url : `${env.apiBaseUrl}/tags/?page=${page}`,
+    );
+  }
+
+  searchPosts({ url, page = 1, query }: SearchParams): Observable<Page<Post>> {
+    return this.http.get<Page<Post>>(
+      url ? url : `${env.apiBaseUrl}/posts/search/?page=${page}&query=${query}`,
+    );
+  }
+}
+
 type ListParams = {
   url?: string;
   page?: number;
@@ -24,65 +78,3 @@ type SearchParams = {
   page?: number;
   query?: string;
 };
-
-// TODO: Implement error handling.
-@Injectable({
-  providedIn: 'root',
-})
-export class PostService {
-  constructor(private http: HttpClient) {}
-
-  public getLatestPosts(): Observable<Page<Post>> {
-    return this.http.get<Page<Post>>(`${env.apiBaseUrl}/posts/latest/`);
-  }
-
-  public getPosts({
-    url,
-    page = 1,
-    tagSlug,
-  }: ListParams): Observable<Page<Post>> {
-    let endpoint = `${env.apiBaseUrl}/posts/?page=${page}`;
-
-    if (tagSlug) {
-      endpoint = `${env.apiBaseUrl}/tags/${tagSlug}/posts/?page=${page}`;
-    }
-
-    return this.http.get<Page<Post>>(url ? url : endpoint);
-  }
-
-  public getPost({ url, year, month, day, slug }: GetParams): Observable<Post> {
-    return this.http.get<Post>(
-      url ? url : `${env.apiBaseUrl}/posts/${year}/${month}/${day}/${slug}/`,
-    );
-  }
-
-  public getSimilarPosts({
-    url,
-    year,
-    month,
-    day,
-    slug,
-  }: GetParams): Observable<Page<Post>> {
-    return this.http.get<Page<Post>>(
-      url
-        ? url
-        : `${env.apiBaseUrl}/posts/${year}/${month}/${day}/${slug}/similar/`,
-    );
-  }
-
-  public getTags({ url, page = 1 }: ListParams): Observable<Page<Tag>> {
-    return this.http.get<Page<Tag>>(
-      url ? url : `${env.apiBaseUrl}/tags/?page=${page}`,
-    );
-  }
-
-  public searchPosts({
-    url,
-    page = 1,
-    query,
-  }: SearchParams): Observable<Page<Post>> {
-    return this.http.get<Page<Post>>(
-      url ? url : `${env.apiBaseUrl}/posts/search/?page=${page}&query=${query}`,
-    );
-  }
-}
