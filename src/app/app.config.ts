d3053345bad5270a7d4985de3ed537b06c8330ca
boonyong <boonyong.kang93@gmail.com>
2024-11-27 21:04:57 +0800

    Revert "Enable routing's scroll restoration"
    This reverts commit 735859ad32e5c3d2a83eaa5a39cdeb4a2e1fe5b2.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,6 @@ import {
   provideRouter,
   TitleStrategy,
   withComponentInputBinding,
-  withInMemoryScrolling,
 } from '@angular/router';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
@@ -22,11 +21,7 @@ import { TemplatePageTitleStrategy } from './shared/utils/template-page-title-st
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(
-      routes,
-      withComponentInputBinding(),
-      withInMemoryScrolling({ scrollPositionRestoration: 'enabled' }),
-    ),
+    provideRouter(routes, withComponentInputBinding()),
     { provide: TitleStrategy, useClass: TemplatePageTitleStrategy },
     provideAnimationsAsync(),
     provideHttpClient(
