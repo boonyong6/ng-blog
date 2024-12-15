30f1fe9d6bc581e4e4248d898ef5f909ec6f8b07
boonyong <boonyong.kang93@gmail.com>
2024-12-15 19:57:54 +0800

    Update path to project root

--- build-for-django.sh
+++ build-for-django.sh
@@ -4,8 +4,8 @@ mv dist/ng-blog/browser/static/* dist/ng-blog/browser/
 rm -r dist/ng-blog/browser/static/
 
 if [ -z "$API_PROJECT_ROOT" ]; then
-  API_PROJECT_ROOT='../blog-api'
+  API_PROJECT_ROOT='../blog-api/mysite'
 fi
 
-rm -r "$API_PROJECT_ROOT/mysite/public/static/dist/"
-cp -r dist/ "$API_PROJECT_ROOT/mysite/public/static/"
+rm -r "$API_PROJECT_ROOT/public/static/dist/"
+cp -r dist/ "$API_PROJECT_ROOT/public/static/"
