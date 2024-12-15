80a512c4dfba36535a265c294e854b362669af80
boonyong <boonyong.kang93@gmail.com>
2024-12-15 21:00:52 +0800

    Clean up bash script

--- build-for-django.sh
+++ build-for-django.sh
@@ -1,11 +1,33 @@
 #!/bin/bash
+echo '=> Building Angular app...'
 npx ng build --deploy-url static/ --configuration staging
-mv dist/ng-blog/browser/static/* dist/ng-blog/browser/
-rm -r dist/ng-blog/browser/static/
+echo '=> End build.'
+echo
+
+echo '=> Reorganizing static files according to Django format...'
+mv ./dist/ng-blog/browser/static/* ./dist/ng-blog/browser/
+rm -r ./dist/ng-blog/browser/static/
+echo '=> End reorganize.'
+echo
 
 if [ -z "$API_PROJECT_ROOT" ]; then
+  echo '=> Setting default value for API_PROJECT_ROOT...'
   API_PROJECT_ROOT='../blog-api/mysite'
+  echo '=> End set.'
+  echo
 fi
 
+echo '=> Removing any existing build artifacts...'
 rm -r "$API_PROJECT_ROOT/public/static/dist/"
-cp -r dist/ "$API_PROJECT_ROOT/public/static/"
+echo '=> End remove.'
+echo
+
+echo '=> Creating directories for build artifacts if not exist...'
+mkdir -p "$API_PROJECT_ROOT/public/static/"
+echo '=> End create.'
+echo
+
+echo '=> Copying build artifacts...'
+cp -r ./dist "$API_PROJECT_ROOT/public/static/"
+echo '=> End copy.'
+echo
